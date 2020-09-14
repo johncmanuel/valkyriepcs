0b4c3c71b85601e4e6f5a9c8e35cf394c9d1d903
john-manuel <jmanuel51801@gmail.com>
2020-09-14 16:08:25 -0700

    Moved meta tags to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import Head from "next/head"
 
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" key="viewport" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" key="http" />
+      </Head>
+      <Component {...pageProps} />
+    </>
 }
 
 export default MyApp
