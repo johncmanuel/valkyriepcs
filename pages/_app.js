31f6ee2714cb35c07de616da8adcd8bca248f1c5
john-manuel <jmanuel51801@gmail.com>
2020-09-14 16:50:15 -0700

    Added title tag

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" key="viewport" />
         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" key="http" />
+        <title>Valkyrie PCs</title>
       </Head>
       <Component {...pageProps} />
     </>
