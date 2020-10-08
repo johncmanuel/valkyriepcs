52c350f0d523a48d032cf55e8f1b9556a35ed00f
john-manuel <jmanuel51801@gmail.com>
2020-10-08 16:29:48 -0700

    Current navbar for now

--- components/nav.js
+++ components/nav.js
@@ -1,31 +1,82 @@
+import { Transition } from '@tailwindui/react';
 import { useState } from 'react';
 
 
-function ToggleButton() {
+function Dropdown() {
     const [isOn, setIsOn] = useState(false);
+    const [inMenu, setInMenu] = useState(false);
     return <>
-        {/* Item active: "text-gray-900", Item inactive: "text-gray-500" */}
         <button
             type="button"
             aria-checked={isOn}
-            onClick={() => setIsOn(!isOn)}
+            onMouseEnter={() => {
+                setIsOn(!isOn);
+                console.log('isOn: ' + !isOn);
+                if (isOn === true) {
+                    console.log('isOn is true, thus inMenu is true!');
+                    setInMenu(!inMenu);
+                }
+            }}
+            onMouseLeave={() => {
+                if (inMenu !== true) {
+                    console.log('inMenu is false, so I must make isOn false.');
+                    setIsOn(false);
+                }
+            }}
             className={`${isOn ? 'text-gray-900' : 'text-gray-500'} inline-flex items-center space-x-2 text-base leading-6 font-medium hover:text-gray-900 focus:outline-none focus:text-gray-900 transition ease-in-out duration-150`}
         >
             <span>Company</span>
-            {/*
-                Heroicon name: chevron-down
-
-                Item active: "text-gray-600", Item inactive: "text-gray-400"
-            */}
-            <svg
-                onClick={() => setIsOn(!isOn)}
-                className={`${isOn ? 'text-gray-600' : 'text-gray-400'} h-5 w-5 group-hover:text-gray-500 group-focus:text-gray-500 transition ease-in-out duration-150`}
-                viewBox="0 0 20 20"
-                fill="currentColor"
-            >
-                <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
-            </svg>
         </button>
+
+        <Transition
+            // Transition component triggered by button
+            show={isOn}
+
+            enter="transition ease-out duration-200"
+            enterFrom="opacity-0 translate-y-1"
+            enterTo="opacity-100 translate-y-0"
+            leave="transition ease-in duration-150"
+            leaveFrom="opacity-100 translate-y-0"
+            leaveTo="opacity-0 translate-y-1"
+            onMouseEnter={() => {
+                setInMenu(!inMenu);
+                console.log('inMenu: ' + inMenu);
+            }}
+            onMouseLeave={() => {
+                setIsOn(false);
+                console.log('inMenu: ' + inMenu);
+            }}
+        >
+            {/* Show desktop menu */}
+            <div className="absolute left-1/2 transform -translate-x-1/2 pt-3 px-2 w-screen max-w-md sm:px-0">
+                <div className="rounded-lg shadow-2xl">
+                    <div className="rounded-lg shadow-xs overflow-hidden">
+                        <div className="z-20 relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+                            <a href="#" className="-m-3 p-3 flex items-start space-x-4 rounded-lg hover:bg-gray-50 transition ease-in-out duration-150">
+                                <div className="space-y-1">
+                                    {/* Create a JSON that tracks all of the 'Company' listings */}
+                                    <p className="text-base leading-6 font-medium text-gray-900">
+                                        About
+                                    </p>
+                                    {/* <p className="text-sm leading-5 text-gray-500">
+                                        
+                                    </p> */}
+                                </div>
+                            </a>
+                            <a href="#" className="-m-3 p-3 flex items-start space-x-4 rounded-lg hover:bg-gray-50 transition ease-in-out duration-150">
+                                <div className="space-y-1">
+                                    <p className="text-base leading-6 font-medium text-gray-900">
+                                        Events
+                                    </p>
+                                    {/* <p className="text-sm leading-5 text-gray-500">
+                                    </p> */}
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Transition>
     </>
 }
 
@@ -55,80 +106,34 @@ export default function Nav() {
                             Contact
                         </a>
                         <div className="relative">
-
-                            <ToggleButton />
-                           
-                            {/*
-                            'More' flyout menu, show/hide based on flyout menu state.
-
-                            Entering: "transition ease-out duration-200"
-                                From: "opacity-0 translate-y-1"
-                                To: "opacity-100 translate-y-0"
-                            Leaving: "transition ease-in duration-150"
-                                From: "opacity-100 translate-y-0"
-                                To: "opacity-0 translate-y-1"
-                            */}
-                            <div className="absolute left-1/2 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-md sm:px-0">
-                                <div className="rounded-lg shadow-lg">
-                                    <div className="rounded-lg shadow-xs overflow-hidden">
-                                        <div className="z-20 relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                                            <a href="#" className="-m-3 p-3 flex items-start space-x-4 rounded-lg hover:bg-gray-50 transition ease-in-out duration-150">
-                                                {/* Heroicon name: support */}
-                                                {/* <svg className="flex-shrink-0 h-6 w-6 text-indigo-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z" />
-                                                </svg> */}
-                                                <div className="space-y-1">
-                                                    <p className="text-base leading-6 font-medium text-gray-900">
-                                                        About
-                                                    </p>
-                                                    <p className="text-sm leading-5 text-gray-500">
-                                                        Get all of your questions answered in our forums or contact support.
-                                                    </p>
-                                                </div>
-                                            </a>
-                                            <a href="#" className="-m-3 p-3 flex items-start space-x-4 rounded-lg hover:bg-gray-50 transition ease-in-out duration-150">
-                                                {/* Heroicon name: calendar */}
-                                                {/* <svg className="flex-shrink-0 h-6 w-6 text-indigo-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                                                </svg> */}
-                                                <div className="space-y-1">
-                                                    <p className="text-base leading-6 font-medium text-gray-900">
-                                                        Events
-                                                    </p>
-                                                    <p className="text-sm leading-5 text-gray-500">
-                                                        See what meet-ups and other events we might be planning near you.
-                                                    </p>
-                                                </div>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            {/* Toggle dropdown menu when clicking on 'Company' */}
+                            <Dropdown />
                         </div>
                     </nav>
-                    <div className="hidden md:flex items-center justify-end space-x-8 md:flex-1 lg:w-0">
+                    {/* <div className="hidden md:flex items-center justify-end space-x-8 md:flex-1 lg:w-0">
                         <a href="#" className="whitespace-no-wrap text-base leading-6 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900">
                             Sign in
-        </a>
+                        </a>
                         <span className="inline-flex rounded-md shadow-sm">
                             <a href="#" className="whitespace-no-wrap inline-flex items-center justify-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150">
                                 Sign up
-          </a>
+                             </a>
                         </span>
-                    </div>
+                    </div> */}
                 </div>
             </div>
-            {/*
-    Mobile menu, show/hide based on mobile menu state.
 
-    Entering: "duration-200 ease-out"
-From: "opacity-0 scale-95"
-To: "opacity-100 scale-100"
-    Leaving: "duration-100 ease-in"
-From: "opacity-100 scale-100"
-To: "opacity-0 scale-95"
-  */}
-            <div className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+            {/*
+                Mobile menu, show/hide based on mobile menu state.
+
+                Entering: "duration-200 ease-out"
+                From: "opacity-0 scale-95"
+                To: "opacity-100 scale-100"
+                    Leaving: "duration-100 ease-in"
+                From: "opacity-100 scale-100"
+                To: "opacity-0 scale-95"
+            */}
+            {/* <div className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
                 <div className="rounded-lg shadow-lg">
                     <div className="rounded-lg shadow-xs bg-white divide-y-2 divide-gray-50">
                         <div className="pt-5 pb-6 px-5 space-y-6">
@@ -138,7 +143,6 @@ To: "opacity-0 scale-95"
                                 </div>
                                 <div className="-mr-2">
                                     <button type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                                        {/* Heroicon name: x */}
                                         <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
                                         </svg>
@@ -148,7 +152,6 @@ To: "opacity-0 scale-95"
                             <div>
                                 <nav className="grid gap-y-8">
                                     <a href="#" className="-m-3 p-3 flex items-center space-x-3 rounded-md hover:bg-gray-50 transition ease-in-out duration-150">
-                                        {/* Heroicon name: chart-bar */}
                                         <svg className="flex-shrink-0 h-6 w-6 text-indigo-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
                                         </svg>
@@ -157,7 +160,6 @@ To: "opacity-0 scale-95"
                 </div>
                                     </a>
                                     <a href="#" className="-m-3 p-3 flex items-center space-x-3 rounded-md hover:bg-gray-50 transition ease-in-out duration-150">
-                                        {/* Heroicon name: cursor-click */}
                                         <svg className="flex-shrink-0 h-6 w-6 text-indigo-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122" />
                                         </svg>
@@ -166,7 +168,6 @@ To: "opacity-0 scale-95"
                 </div>
                                     </a>
                                     <a href="#" className="-m-3 p-3 flex items-center space-x-3 rounded-md hover:bg-gray-50 transition ease-in-out duration-150">
-                                        {/* Heroicon name: shield-check */}
                                         <svg className="flex-shrink-0 h-6 w-6 text-indigo-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
                                         </svg>
@@ -175,7 +176,6 @@ To: "opacity-0 scale-95"
                 </div>
                                     </a>
                                     <a href="#" className="-m-3 p-3 flex items-center space-x-3 rounded-md hover:bg-gray-50 transition ease-in-out duration-150">
-                                        {/* Heroicon name: view-grid */}
                                         <svg className="flex-shrink-0 h-6 w-6 text-indigo-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
                                         </svg>
@@ -184,7 +184,6 @@ To: "opacity-0 scale-95"
                 </div>
                                     </a>
                                     <a href="#" className="-m-3 p-3 flex items-center space-x-3 rounded-md hover:bg-gray-50 transition ease-in-out duration-150">
-                                        {/* Heroicon name: refresh */}
                                         <svg className="flex-shrink-0 h-6 w-6 text-indigo-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
                                         </svg>
@@ -238,7 +237,7 @@ To: "opacity-0 scale-95"
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> */}
         </div>
 
     </>
