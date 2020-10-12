a505a58abbbc204068d4d236c9c1d7b992318abf
john-manuel <jmanuel51801@gmail.com>
2020-10-12 14:00:24 -0700

    import link for nav

--- components/nav.js
+++ components/nav.js
@@ -1,5 +1,6 @@
-import { Transition } from '@tailwindui/react';
-import { useState } from 'react';
+import { Transition } from '@tailwindui/react'
+import { useState } from 'react'
+import Link from 'next/link'
 
 
 function Dropdown() {
@@ -86,9 +87,11 @@ export default function Nav() {
             <div className="max-w-7xl mx-auto px-4 sm:px-6">
                 <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
                     <div className="lg:w-0 lg:flex-1">
-                        <a href="#" className="flex text-base leading-6 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition ease-in-out duration-150">
-                            Valkyrie PCs
-                        </a>
+                        <Link href="/">
+                            <a className="flex text-base leading-6 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition ease-in-out duration-150">
+                                Valkyrie PCs
+                            </a>
+                        </Link>
                     </div>
                     <div className="-mr-2 -my-2 md:hidden">
                         <button type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
