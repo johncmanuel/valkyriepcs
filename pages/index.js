67ada9b58f09801b1a8cf6c4abde8e0747699fee
john-manuel <jmanuel51801@gmail.com>
2020-10-12 14:23:17 -0700

    homepage update

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,63 @@
-import Footer from 'components/footer'
-import Nav from 'components/nav'
-import Header from 'components/header'
+import Footer from 'components/Footer'
+import Nav from 'components/Nav'
+import Header from 'components/Header'
 
 
 export default function Home() {
   return <>
-    <Header></Header>
-    <Nav></Nav>
+    <Header />
+    <Nav />
 
-    <section className="pt-10 pb-11">
+    {/*  */}
+
+    {/* Hero Section */}
+    <section className="text-gray-700 body-font">
+      <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+              Valkyrie PCs
+              <br className="hidden lg:inline-block" />Building computers for anyone for any price
+            </h1>
+          <div className="flex justify-center">
+            <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+              Get started
+            </button>
+            {/* <button className="ml-4 inline-flex text-gray-700 bg-gray-200 border-0 py-2 px-6 focus:outline-none hover:bg-gray-300 rounded text-lg">
+              
+            </button> */}
+          </div>
+        </div>
+        {/* <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
+        </div> */}
+      </div>
+    </section>
+
+    {/* CTA Section */}
+    <section className="pt-10 pb-10">
       <div className="bg-gray-50">
         <div className="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
           <h2 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
             Ready for a new PC?
-      <br />
-            <span className="text-indigo-600">Let us build it.</span>
+          <br />
+            <span className="text-indigo-600">Let's build it.</span>
           </h2>
           <div className="mt-8 flex lg:flex-shrink-0 lg:mt-0">
             <div className="inline-flex rounded-md shadow">
               <a href="#" className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
                 Get started
-        </a>
+              </a>
             </div>
-            <div className="ml-3 inline-flex rounded-md shadow">
+            {/* <div className="ml-3 inline-flex rounded-md shadow">
               <a href="#" className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-600 bg-white hover:text-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
                 Learn more
-        </a>
-            </div>
+              </a>
+            </div> */}
           </div>
         </div>
       </div>
-
     </section>
 
-    <Footer></Footer>
+    <Footer />
   </>
 }
