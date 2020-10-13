6d081b4c582e4cc30411aa84bd430150e086ebbf
john-manuel <jmanuel51801@gmail.com>
2020-10-13 15:03:01 -0700

    updated homepage

--- pages/index.js
+++ pages/index.js
@@ -8,29 +8,30 @@ export default function Home() {
     <Header />
     <Nav />
 
-    {/*  */}
-
     {/* Hero Section */}
-    <section className="text-gray-700 body-font">
-      <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+    <section className="pb-16 pt-16">
+      <article className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+          <header>
+            <h1 className="sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
               Valkyrie PCs
-              <br className="hidden lg:inline-block" />Building computers for anyone for any price
             </h1>
-          <div className="flex justify-center">
-            <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-              Get started
-            </button>
-            {/* <button className="ml-4 inline-flex text-gray-700 bg-gray-200 border-0 py-2 px-6 focus:outline-none hover:bg-gray-300 rounded text-lg">
-              
-            </button> */}
-          </div>
+            <br className="inline-block" />
+            <h3 className="sm:text-2xl text-xl mb-4 font-medium text-gray-900">
+              Building computers for anyone at any price.
+            </h3>
+          </header>
+          <br className="inline-block" />
         </div>
+          <div className="flex justify-center">
+            <button className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-full text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
+              Get Started
+            </button>
+          </div>
         {/* <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
           <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
         </div> */}
-      </div>
+      </article>
     </section>
 
     {/* CTA Section */}
@@ -43,9 +44,9 @@ export default function Home() {
             <span className="text-indigo-600">Let's build it.</span>
           </h2>
           <div className="mt-8 flex lg:flex-shrink-0 lg:mt-0">
-            <div className="inline-flex rounded-md shadow">
-              <a href="#" className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
-                Get started
+            <div className="inline-flex">
+              <a href="#" className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-full text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
+                Get Started
               </a>
             </div>
             {/* <div className="ml-3 inline-flex rounded-md shadow">
