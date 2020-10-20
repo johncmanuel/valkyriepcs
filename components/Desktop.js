6c2131b28d6f7eada0e3eac04c41e1f2fbe4c82a
john-manuel <jmanuel51801@gmail.com>
2020-10-19 17:10:42 -0700

    created desktop svg component

---
+++ components/Desktop.js
@@ -0,0 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDesktop } from "@fortawesome/free-solid-svg-icons";
+
+export default function Desktop() {
+  return <FontAwesomeIcon icon={faDesktop} size="3x" />;
+}
