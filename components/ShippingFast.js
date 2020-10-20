9b23dcbd2f25dde8abd3f3ed13c871d5df3b7467
john-manuel <jmanuel51801@gmail.com>
2020-10-19 17:10:55 -0700

    created delivery truck svg icon

---
+++ components/ShippingFast.js
@@ -0,0 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShippingFast } from "@fortawesome/free-solid-svg-icons";
+
+export default function ShippingFast() {
+  return <FontAwesomeIcon icon={faShippingFast} size="3x" />;
+}
