bdb5319cfaf4c1bd3dcd8560fffe45c0964424a5
john-manuel <jmanuel51801@gmail.com>
2020-10-22 15:37:33 -0700

    using google fonts for open sans font

--- components/header.js
+++ components/header.js
@@ -4,8 +4,15 @@ export default function Header() {
   return (
     <Head>
       {/* Fonts */}
-      {/* <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&display=swap" rel="stylesheet" /> */}
+      {/* <link
+        href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&display=swap"
+        rel="stylesheet"
+      /> */}
       <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"
+        rel="stylesheet"
+      />
     </Head>
   );
 }
