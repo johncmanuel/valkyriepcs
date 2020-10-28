3bb5b8a8861517238d14df3849f0529d7cc02f37
john-manuel <jmanuel51801@gmail.com>
2020-10-28 15:06:30 -0700

    changed variable name to camelCase convention

--- components/Container.js
+++ components/Container.js
@@ -1,6 +1,6 @@
 export default function Container({ children, ...classes }) {
-  const local_classes = classes.classes;
-  if (local_classes === undefined) {
+  const localClasses = classes.classes;
+  if (localClasses === undefined) {
     return <div className="container mx-auto px-4 sm:px-6">{children}</div>;
   } else {
     return (
