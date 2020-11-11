fa94d5ebeaa99c2f71d64f1db4e2690bb0377961
john-manuel <jmanuel51801@gmail.com>
2020-11-10 16:22:35 -0800

    moved icon components into icon folders

---
+++ components/icons/Blog.js
@@ -0,0 +1,8 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBlog } from "@fortawesome/free-solid-svg-icons";
+
+export default function BlogIcon() {
+  return (
+    <FontAwesomeIcon icon={faBlog} fontSize="36px" size="sm" color="#5850ec" />
+  );
+}
---
+++ components/icons/Flag.js
@@ -0,0 +1,8 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFlag } from "@fortawesome/free-solid-svg-icons";
+
+export default function Flag() {
+  return (
+    <FontAwesomeIcon icon={faFlag} fontSize="36px" size="sm" color="#5850ec" />
+  );
+}
