96e2dcf2a49cb424d677415c5a3e42b09c909080
john-manuel <jmanuel51801@gmail.com>
2020-11-11 14:31:36 -0800

    changed size of icons

--- components/icons/BlogIcon.js
+++ components/icons/BlogIcon.js
@@ -2,7 +2,5 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBlog } from "@fortawesome/free-solid-svg-icons";
 
 export default function BlogIcon() {
-  return (
-    <FontAwesomeIcon icon={faBlog} fontSize="36px" size="sm" color="#5850ec" />
-  );
+  return <FontAwesomeIcon icon={faBlog} size="lg" color="#5850ec" />;
 }
--- components/icons/Flag.js
+++ components/icons/Flag.js
@@ -2,7 +2,5 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFlag } from "@fortawesome/free-solid-svg-icons";
 
 export default function Flag() {
-  return (
-    <FontAwesomeIcon icon={faFlag} fontSize="36px" size="sm" color="#5850ec" />
-  );
+  return <FontAwesomeIcon icon={faFlag} size="lg" color="#5850ec" />;
 }
