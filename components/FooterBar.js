eb6132ccbfe0aa61786ad73736ebb08fa29534db
john-manuel <jmanuel51801@gmail.com>
2020-11-19 13:40:06 -0800

    added dummy img, updated email

--- components/FooterBar.js
+++ components/FooterBar.js
@@ -7,10 +7,19 @@ export default function Footer() {
       <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-no-wrap flex-wrap flex-col">
         <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
           <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-            {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-                            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                        </svg> */}
-            <p>img</p>
+            {/* Placeholder image */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+            </svg>
             <span className="ml-3 text-xl">{COMPANY.name}</span>
           </a>
           <p className="mt-2 text-sm text-gray-500">{COMPANY.motto}</p>
@@ -89,7 +98,7 @@ export default function Footer() {
                   className="text-gray-600 hover:text-gray-800"
                   target="_blank"
                   rel="noopener noreferrer"
-                  href="#"
+                  href={COMPANY.email}
                 >
                   Email
                 </a>
